27d0ddcb03a63df22541d8ace0972becb35d96e8
Ji Eun Kim <J.Kim54@lse.ac.uk>
2020-03-11 17:57:32 +0900

    Perform chi-squared tests

--- chi_sq_tests.R
+++ chi_sq_tests.R
@@ -1,13 +1,12 @@
 library(pscl)
 
 
-# Examine the distribution of numbers from 100 simulations 
+# Examine the distribution of motifs from 200 simulations 
 # to test whether the assumption of a Poisson distribution is appropriate.
 
 
 cal_dpois <- function(row_num, df){
-  # Calculate the poisson fitted probability.
-  vals <- as.numeric(df[row_num, 3:(ncol(df)-2)])
+  vals <- as.numeric(df[row_num, 4:(ncol(df)-3)])
   stats <- as.data.frame(table(vals), stringsAsFactors = FALSE)
   stats$vals <- as.numeric(stats$vals)
   stats$poisson <- round(dpois(stats$vals, df$rand_mean[row_num]), 3)
@@ -16,7 +15,9 @@ cal_dpois <- function(row_num, df){
 }
 
 
-get_chisq_p_val <- function(data){
+run_pois_chi_sq_tests <- function(data, output_filename) {
+  data$pois_pval <- NA
+  
   for (i in c(1: nrow(data))) {
     test_tab <- cal_dpois(i, data)
     obs_sum <- sum(test_tab$freq)
@@ -25,61 +26,19 @@ get_chisq_p_val <- function(data){
     
     # Rescale the hypothesised probabilities to make sum(hyp_prob) = 1.
     chisq_gof_test <- chisq.test(test_tab$freq, p=hyp_prob, rescale.p = TRUE)
-    # chisq_gof_test$observed
-    # chisq_gof_test$expected
     data$pois_pval[i] <- round(chisq_gof_test$p.value, 3)
-    print(chisq_gof_test)
   } 
+  write.csv(data, output_filename, row.names = FALSE)
 }
 
 
-dat <- read.csv('data/chi_sq/simple_motifs.csv', stringsAsFactors = FALSE)
-dat$rand_mean <- rowMeans(dat[, 3:ncol(dat)])
-dat$pois_pval <- NA
-get_chisq_p_val(dat)
-
-
-dat <- read.csv('data/chi_sq/strict_motifs.csv', stringsAsFactors = FALSE)
-dat$rand_mean <- rowMeans(dat[, 3:ncol(dat)])
-dat$pois_pval <- NA
-get_chisq_p_val(dat)
-
-
-# Plot of Poisson Distribution: Observed vs. Expected
-# Observed frequency
-plot(as.numeric(test_tab$val), chisq_gof_test$observed, 
-     main="Poisson Distribution: Observed vs. Expected", 
-     type='b', 
-     pch=0,
-     col='blue',
-     xlab="Number of strict (1, 1) motifs",
-     ylab="Frequency",
-     ylim=c(0,9))
-
-# Dual Y-axes plot (secondary Y-axis)
-par(new=T)
-
-# Expected frequency
-plot(as.numeric(test_tab$val), chisq_gof_test$expected, 
-     type='b', 
-     pch=1,
-     col='red', 
-     xlab="", 
-     ylab="",
-     ylim=c(0,9))
-
-legend(x=305, y=9, 
-       c("Observed", "Expected"), 
-       pch=c(0,1), 
-       col=c('blue', 'red'))
-
-
-# Examine the distribution of numbers from 100 simulations 
+# Examine the distribution of motifs from 200 simulations 
 # to test whether the assumption of a Normal distribution is appropriate.
 
 
 cal_dnorm <- function(row_num, df){
-  vals <- as.numeric(df[row_num, 3:(ncol(df)-3)])
+  # Calculate the poisson fitted probability.
+  vals <- as.numeric(df[row_num, 4:(ncol(df)-3)])
   stats <- as.data.frame(table(vals), stringsAsFactors = FALSE)
   stats$vals <- as.numeric(stats$vals)
   stats$density <- round(dnorm(stats$vals, df$rand_mean[row_num], 
@@ -89,27 +48,26 @@ cal_dnorm <- function(row_num, df){
 }
 
 
-get_chisq_norm_p_val <- function(data) {
+run_norm_chi_sq_tests <- function(data, output_filename) {
+  data$norm_pval <- NA
+  
   for (i in c(1: nrow(data))) {
     test_tab <- cal_dnorm(i, data)
-    print(sum(test_tab$norm_density))
+    
     # Rescale the hypothesised probabilities to make sum(hyp_prob) = 1.
-    chisq_gof_test <- chisq.test(test_tab$freq, p=test_tab$norm_density, 
-                                 rescale.p = TRUE)
-    # chisq_gof_test$observed
-    # chisq_gof_test$expected
+    chisq_gof_test <- chisq.test(test_tab$freq, p=test_tab$norm_density, rescale.p = TRUE)
     data$norm_pval[i] <- round(chisq_gof_test$p.value, 3)
-    print(chisq_gof_test)
   } 
+  write.csv(data, output_filename, row.names = FALSE)
 }
 
 
-dat <- read.csv('data/chi_sq/simple_motifs_norm_dat.csv', stringsAsFactors = FALSE)
-dat$norm_pval <- NA
-get_chisq_norm_p_val(dat)
+data <- read.csv("data/chi_sq/RO_RE_chi_sq_data.csv", stringsAsFactors = FALSE)
+run_pois_chi_sq_tests(data, "data/chi_sq/RO_RE_chi_sq_poiss.csv")
+run_norm_chi_sq_tests(data, "data/chi_sq/RO_RE_chi_sq_norm.csv")
 
 
-dat <- read.csv('data/chi_sq/strict_motifs_norm_dat.csv', stringsAsFactors = FALSE)
-dat$norm_pval <- NA
-get_chisq_norm_p_val(dat)
+data <- read.csv("data/chi_sq/SO_SE_chi_sq_data.csv", stringsAsFactors = FALSE)
+run_pois_chi_sq_tests(data, "data/chi_sq/SO_SE_chi_sq_poiss.csv")
+run_norm_chi_sq_tests(data, "data/chi_sq/SO_SE_chi_sq_norm.csv")
 
